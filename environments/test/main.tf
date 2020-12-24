c35278c4154f99a9cbf3f5cb9c0c304c7124e55a
Shubham Wazir <wazirshubham@outlook.com>
2020-12-24 17:14:20 +0530

    1st commit

---
+++ environments/test/main.tf
@@ -0,0 +1,33 @@
+provider "azurerm" {
+  tenant_id       = "${var.tenant_id}"
+  subscription_id = "${var.subscription_id}"
+  client_id       = "${var.client_id}"
+  client_secret   = "${var.client_secret}"
+  features {}
+}
+terraform {
+  backend "azurerm" {
+    storage_account_name = "tstate11724"
+    container_name       = "tstate"
+    access_key           = "dDOCw6SS1kA+1MZknscQp5pxUVfXo6U3H3m58+9f1VPlwiJ2hGGO3tcuhywiNEx6HGKLK9Otf5Re87E+Yc0oDw=="
+  }
+}
+locals {
+  tags = {
+    tier        = "${var.tier}"
+    deployment  = "${var.deployment}"
+  }
+}
+module "resource_group" {
+  source               = "../../modules/resource_group"
+  resource_group       = "${var.resource_group}"
+  location             = "${var.location}"
+}
+module "appservice" {
+  source           = "../../modules/appservice"
+  location         = "${var.location}"
+  application_type = "${var.application_type}"
+  resource_type    = "AppService"
+  resource_group   = "${module.resource_group.resource_group_name}"
+  tags             = "${local.tags}"
+}
---
+++ environments/test/terraform.tfvars
@@ -0,0 +1,14 @@
+# Azure subscription vars
+subscription_id = "1b38ca68-b84b-4c34-b3cd-2ec292380a78"
+client_id = "13e97fbe-81af-40cb-ac6e-70b2dfd77875"
+client_secret = "9_8dJh1C_54..rVHe720TC7Jm28q-4i4ua"
+tenant_id = "54874cd9-7379-48cc-a9c6-cb1ca3c1bc6e"
+
+# Resource Group/Location
+location = "East US 2"
+resource_group = "Lesson1"
+application_type = "myApplication31387"
+
+# Tags
+tier = "Test"
+deployment = "Terraform"
